97a0f2f553f44192adcb434d8f9b371171455bc0
2oothpick <kobbyboateng14@gmail.com>
2024-01-28 21:02:43 +0000

    Add models to admin.py

--- BookWander/Wanderapp/admin.py
+++ BookWander/Wanderapp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from . import models 
 # Register your models here.
+admin.site.register(models.User)
+admin.site.register(models.Book)
+admin.site.register(models.Order)
+admin.site.register(models.OrderItem)
+admin.site.register(models.UserPreferences)
+admin.site.register(models.Review)
+admin.site.register(models.Feedback)
+admin.site.register(models.ShoppingCart)
+admin.site.register(models.CartItem)
+admin.site.register(models.Subscription)
+admin.site.register(models.Image)
