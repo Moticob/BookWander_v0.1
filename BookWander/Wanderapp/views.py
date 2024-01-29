285408309aa726726b799e403a2f987290f255aa
2oothpick <kobbyboateng14@gmail.com>
2024-01-29 18:08:50 +0000

    Add View for initial homepage

--- BookWander/Wanderapp/views.py
+++ BookWander/Wanderapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import *
+from django.http import HttpResponse
 # Create your views here.
+
+def homepage(request):
+    """View for homepage"""
+    return HttpResponse("Welcome to BookWander")
