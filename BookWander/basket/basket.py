caee9328462acef1b17d8c448e8b4973233cc36b
2oothpick <kobbyboateng14@gmail.com>
2024-02-01 01:18:46 +0000

    Adding items to basket function

--- BookWander/basket/basket.py
+++ BookWander/basket/basket.py
@@ -14,3 +14,20 @@ class Basket():
         if 'skey' not in request.session:
             basket = self.session['skey'] = {'number':123123}
         self.basket = basket
+
+    
+    def add(self, book, qty):
+        """
+        Adding and updating the user's basket session data
+        """
+        book_id = book.id
+
+        if book_id not in self.basket:
+            self.basket[book_id] = {'price': str( book.price), 'qty':int(qty)}
+        self.session.modified = True
+
+    def __len__(self):
+        """
+        Gets basket data and counts qty of items
+        """
+        return sum(item['qty'] for item in self.basket.values())
