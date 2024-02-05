0de18cc55d667c9452520470b46c34767ea07397
2oothpick <kobbyboateng14@gmail.com>
2024-02-05 17:35:52 +0000

    Add account urls to project

--- BookWander/BookWander/urls.py
+++ BookWander/BookWander/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Wanderapp.urls', namespace='Wanderapp')),
     path('basket/', include('basket.urls', namespace='basket')),
+    path('account/', include('account.urls', namespace='account')),
 ]
 
 if settings.DEBUG:
